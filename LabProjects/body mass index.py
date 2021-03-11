69c9c13d384338f0a136a21dc24c52a0590a7ecb
TheARJ <aayushrajjoshi4@gmail.com>
2021-03-11 19:01:03 +0545

    program

--- LabProjects/body mass index.py
+++ LabProjects/body mass index.py
@@ -1,4 +1,4 @@
-"""Solve each of the following problems using Python Scripts. Make sure you use appropriate variable names and comments.
+"""6. Solve each of the following problems using Python Scripts. Make sure you use appropriate variable names and comments.
 When there is a final answer have Python print it to the screen.
  A person’s body mass index (BMI) is defined as:
  BMI=mass in kg / (height in m)^2.

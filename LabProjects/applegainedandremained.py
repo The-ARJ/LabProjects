294056a5ccf8df81ee9cbe571b40359d96c39a81
TheARJ <aayushrajjoshi4@gmail.com>
2021-03-11 19:00:54 +0545

    program

--- LabProjects/applegainedandremained.py
+++ LabProjects/applegainedandremained.py
@@ -1,4 +1,4 @@
-"""N students  take k apples and distribute them among each other evenly.
+"""3. N students  take k apples and distribute them among each other evenly.
 The remaining (the non divisible) part remains in the basket . How many apples will each single student get?
 How many apples will remain in the basket? The program reads the numbers N and K. It should print the two answers for
 the question above."""

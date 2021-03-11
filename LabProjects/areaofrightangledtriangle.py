b0fc09edda322e5be78ba5b1a7f7b1ecbc76c2de
TheARJ <aayushrajjoshi4@gmail.com>
2021-03-11 18:59:02 +0545

    program

--- LabProjects/areaofrightangledtriangle.py
+++ LabProjects/areaofrightangledtriangle.py
@@ -1,8 +1,8 @@
-"""Write a program that reads the length of the base and height of a right angle triangle
+"""2. Write a program that reads the length of the base and height of a right angle triangle
 and print the area.Every number is given in the separate line"""
 
-a=float(input("enter value fo the base:"))
-b=float(input("enter value for height:"))
-area=(a*b)//2
-print("The area of rigt angle triangle is:",area)
+a = float(input("enter value fo the base:"))
+b = float(input("enter value for height:"))
+area = (a*b)//2
+print("The area of rigt angle triangle is:", area)
 
